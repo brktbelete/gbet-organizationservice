af3b592db5d10b1d81694972d70ae5459f64a6e1
Bereket Belete <bbelete@apposit.com>
2020-04-18 18:06:57 +0300

    initial commit

---
+++ src/main/java/com/gbet/organizationservice/controller/OrganizationController.java
@@ -0,0 +1,20 @@
+package com.gbet.organizationservice.controller;
+
+import com.gbet.organizationservice.model.Organization;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/organizations/{organizationId}")
+public class OrganizationController {
+
+    @GetMapping
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId){
+
+        Organization organization = new Organization();
+        organization.setId("orgServiceID");
+        return organization;
+    }
+}
---
+++ src/main/java/com/gbet/organizationservice/model/Organization.java
@@ -0,0 +1,15 @@
+package com.gbet.organizationservice.model;
+
+public class Organization {
+
+    String id;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+
+        return this.id;
+    }
+}
